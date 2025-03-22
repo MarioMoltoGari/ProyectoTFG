1918b4f6559fe9e5c71ac60433ebf39ea0fb5f7c
Mario Moltó Garí <mmoltogari@gmail.com>
2025-03-22 15:33:37 +0100

    Commit final

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Función para desplegar y contraer el menú
 function toggleMenu() {
+<<<<<<< HEAD
     const menu = document.getElementById("dropdown-menu");
     const buttonContainer = document.getElementById("menu-container");
     menu.classList.toggle("show");
     buttonContainer.classList.toggle("show");
+=======
+    const menuContainer = document.getElementById('menu-container');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    menuContainer.classList.toggle('show');
+    dropdownMenu.classList.toggle('show');
+>>>>>>> bb5f9e1 (Primer modal de inicio de sesión 22/03/2025)
 }
 
 // Función para mostrar el menú contextual
@@ -29,4 +36,27 @@ document.querySelectorAll('.test-link').forEach(link => {
 });
 
 // Ocultar el menú contextual al hacer clic en cualquier lugar de la página
+<<<<<<< HEAD
 document.addEventListener('click', hideContextMenu);
+=======
+document.addEventListener('click', hideContextMenu);
+
+function openModal() {
+    document.getElementById('login-modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('login-modal').style.display = 'none';
+}
+
+// Agregar evento al botón de iniciar sesión para abrir el modal
+document.querySelector('.login-button').addEventListener('click', openModal);
+
+// Cerrar el modal cuando se hace clic fuera del contenido del modal
+window.onclick = function(event) {
+    const modal = document.getElementById('login-modal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+>>>>>>> bb5f9e1 (Primer modal de inicio de sesión 22/03/2025)
